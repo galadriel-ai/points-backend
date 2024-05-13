169ef21baa951527cbab3f748504676a50aac017
kgrofelnik <kresimir.grofelnik@gmail.com>
2024-05-13 10:32:15 +0200

    Fixes db migration

--- database/alembic/versions/000000000010_adds_discord_username_and_id.py
+++ database/alembic/versions/000000000010_adds_discord_username_and_id.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '000000000007'
-down_revision = '000000000006'
+revision = '0000000000010'
+down_revision = '000000000009'
 branch_labels = None
 depends_on = None
 
